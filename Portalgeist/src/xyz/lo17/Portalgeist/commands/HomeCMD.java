a19f9dbf473604c91f86342a26a960a50a28b538
Guzio <star.master.123.mg@gmail.com>
2021-12-24 02:49:08 +0100

    /home gotowe!

--- Portalgeist/src/xyz/lo17/Portalgeist/commands/HomeCMD.java
+++ Portalgeist/src/xyz/lo17/Portalgeist/commands/HomeCMD.java
@@ -1,9 +1,11 @@
 package xyz.lo17.Portalgeist.commands;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import org.jetbrains.annotations.NotNull;
 
 public class HomeCMD implements CommandExecutor{
@@ -13,7 +15,13 @@ public class HomeCMD implements CommandExecutor{
         if (!(sender instanceof Player)) return false;
         Player player = (Player)sender;
 
-        //TODO: Znaleźć koordy i tepnąć na nie gracza.
+        Location spawn = player.getBedLocation();
+        if (spawn == null){
+            player.sendMessage("Brak domu. Użyj /sethome!");
+            return true;
+        }
+
+        player.teleport(spawn, TeleportCause.COMMAND);
         
         return true;
     }
