a7f29ee3ce68d1c0d8d8ff32c57fa5991b8a1e47
Guzio <star.master.123.mg@gmail.com>
2021-12-24 02:59:47 +0100

    Jescze koordów muszę szukać :sob:...

--- Portalgeist/src/xyz/lo17/Portalgeist/commands/SpawnCMD.java
+++ Portalgeist/src/xyz/lo17/Portalgeist/commands/SpawnCMD.java
@@ -1,9 +1,12 @@
 package xyz.lo17.Portalgeist.commands;
 
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import org.jetbrains.annotations.NotNull;
 
 public class SpawnCMD implements CommandExecutor{
@@ -13,7 +16,15 @@ public class SpawnCMD implements CommandExecutor{
         if (!(sender instanceof Player)) return false;
         Player player = (Player)sender;
 
-        //TODO: Znaleźć koordy i tepnąć na nie gracza.
+        double x = 0;
+        double y = 104;
+        double z = 0;
+        float pitch = 0;
+        float yaw = 0;
+        World world = sender.getServer().getWorld("Spawn");
+
+        Location location = new Location(world, x, y, z, pitch, yaw);
+        player.teleport(location, TeleportCause.COMMAND);
         
         return true;
     }
