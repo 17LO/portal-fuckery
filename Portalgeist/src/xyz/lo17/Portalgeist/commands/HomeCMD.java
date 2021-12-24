15c72926a586b8abb6645c225a1da7dc4c6d24a0
Guzio <star.master.123.mg@gmail.com>
2021-12-24 02:44:11 +0100

    Przygotowano bazę pod komendy "statycznego" TP.

--- Portalgeist/src/xyz/lo17/Portalgeist/commands/HomeCMD.java
+++ Portalgeist/src/xyz/lo17/Portalgeist/commands/HomeCMD.java
@@ -3,14 +3,19 @@ package xyz.lo17.Portalgeist.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class HomeCMD implements CommandExecutor{
 
     @Override
-    public boolean onCommand(@NotNull CommandSender arg0, @NotNull Command arg1, @NotNull String arg2, @NotNull String[] arg3) {
-        // TODO Auto-generated method stub
-        return false;
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command arg1, @NotNull String arg2, @NotNull String[] arg3) {
+        if (!(sender instanceof Player)) return false;
+        Player player = (Player)sender;
+
+        //TODO: Znaleźć koordy i tepnąć na nie gracza.
+        
+        return true;
     }
     
 }
